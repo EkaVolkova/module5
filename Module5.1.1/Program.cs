6217202447704fe6ebc2f6b75c3206da4b53af04
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-13 16:49:16 +0500

    5.1.5 Напишите программу, которая в цикле вызывает метод ShowColor(), записывает его значение в массив из трех цветов favcolors, а потом отображает значения этого массива.

---
+++ Module5.1.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Module5._1._5
+{
+    class Program
+    {
+        static string ShowColors()
+        {
+			string color = Console.ReadLine();
+			switch (color)
+			{
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is red!");
+					break;
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is green!");
+					break;
+				case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your color is yellow!");
+					break;
+			}
+			return color;
+		}
+
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 Ваших любимых цвета");
+			var favColors = new string[3];
+            for (int i = 0; i < favColors.Length; i++)
+            {
+				favColors[i] = ShowColors();
+            }
+            Console.WriteLine("Ваши любимые цвета:");
+			foreach(var color in favColors)
+            {
+                Console.WriteLine(color);
+            }
+		}
+	}
+}
