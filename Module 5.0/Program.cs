c57cabe1e36cf4fc4aa747b48e2ced820cfc31d2
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-13 16:43:24 +0500

    Создайте кортеж User, содержащий имя пользователя Name и массив с текстовой информацией о его пяти любимых блюдах Dishes. Заполните имя пользователя через ввод в консоль, а информацию о блюдах — в цикле через консоль

---
+++ Module 5.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module_5._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Delishes) User;
+
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+            User.Delishes = new string[5];
+            Console.WriteLine("Введите пять любимых блюд через Enter");
+            for (int i = 0; i < 5; i++)
+            {
+                User.Delishes[i] = Console.ReadLine();
+            }
+        }
+    }
+}
