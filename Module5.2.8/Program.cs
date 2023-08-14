3dca971a4c7cb25e074bb986f09b5c88b1573a3a
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-14 19:51:40 +0500

    5.2.8 Разделить метод из задания 5.1.6 GetArrayFromConsole() на два: один метод GetArrayFromConsole() должен читать введенные с клавиатуры массивы и возвращать их, второй метод SortArray() должен принимать параметром массив array типа данных int, сортировать его и возвращать

---
+++ Module5.2.8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Module5._2._8
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            
+            return result;
+        }
+        static int[] SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int k = array[i];
+                        array[i] = array[j];
+                        array[j] = k;
+                    }
+                }
+            }
+            
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            
+            int[] array = SortArray(GetArrayFromConsole());
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
