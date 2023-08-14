5f99af1a6a13925f638dff3e818f91e71e0d0be8
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-14 19:44:02 +0500

    5.2.7 имя и возраст выводятся одной строкой, а "Напишите свой любимый цвет" — другой.

--- Module5.2/Program.cs
+++ Module5.2/Program.cs
@@ -4,9 +4,9 @@ namespace Module5._2
 {
     class Program
     {
-		static string ShowColors(string name)
+		static string ShowColors(string name, int age)
 		{
-			Console.WriteLine("{0}, напишите свой любимый цвет на английском с маленькой буквы", name);
+			Console.WriteLine("{0}, {1}\nНапишите свой любимый цвет на английском с маленькой буквы", name, age);
 			string color = Console.ReadLine();
 			switch (color)
 			{
@@ -52,7 +52,7 @@ namespace Module5._2
 			var favColors = new string[3];
 			foreach(var color in favColors)
             {
-				ShowColors(anketa.name);
+				ShowColors(anketa.name, anketa.age);
             }
             Console.ReadKey();
         }
